0ac110addb990d0544f830a89e4db584417a5fc1
yzavyalov <8540462@gmail.com>
2023-10-01 11:05:32 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+    <script src="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>chargeback reduction system for high-risk</title>
+</head>
+<body>
+<div class="container-fluid">
+    <header>
+        <div class="row header">
+            <div class="col">
+                <p>kfdjghkfdhk</p>
+            </div>
+        </div>
+    </header>
+    <div class="row">
+        <div class="col">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A facilis maxime odit, quae quisquam quo sunt vero? Laborum nesciunt quaerat sint voluptas. Adipisci fugit hic rerum? Doloremque ratione similique voluptate.</p>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="row footer">
+        <div class="col">
+            <p>fdgnkfd</p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
